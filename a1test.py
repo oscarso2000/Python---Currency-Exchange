c5cbdc55cb3843dcfbab74a66371d9225f14d9d1
Oscar <os2304@columbia.edu>
2019-09-11 16:33:26 -0400

    second

---
+++ a1test.py
@@ -0,0 +1,44 @@
+"""
+Unit test for module a1
+
+When run as a script, this module invokes several procedures that 
+test the various functions in the module a1.
+
+Author: Oscar So (ons4)
+Date:   September 11, 2019
+"""
+import introcs
+import a1
+
+def testA():
+    """
+    Test procedure for Part A
+    """
+    introcs.assert_equals('0.863569', a1.before_space("0.863569 Euros"))
+    introcs.assert_equals('Euros', a1.after_space("0.8663569 Euros"))
+    #introcs.assert_equals(...)
+    
+
+def testB():
+     """
+     Test procedure for Part B
+     """
+     pass
+
+def testC():
+     """
+     Test procedure for Part C
+     """
+     pass
+
+def testD():
+     """
+     Test procedure for Part D
+     """
+     pass
+
+testA()
+testB()
+testC()
+testD()
+print("Module a1 passed all tests")
